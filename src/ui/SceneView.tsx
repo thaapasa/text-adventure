ad884d4f90e79bc176033108f9e178612a438e28
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-12-25 19:23:21 +0200

    Add markdown rendering to scene description text

--- src/ui/SceneView.tsx
+++ src/ui/SceneView.tsx
@@ -4,6 +4,7 @@ import { Scene, Game } from '../data/Game';
 import { ImageTile } from './ImageTile';
 import { Link } from 'react-router-dom';
 import { gameService } from '../data/GameService';
+import * as Markdown from 'react-markdown';
 
 export class SceneView extends React.Component<{
   readonly game: Game;
@@ -13,7 +14,7 @@ export class SceneView extends React.Component<{
     return (
       <ImageTile className="Scene" url={this.props.scene.image}>
         <div className="Header"><h3>{this.props.scene.name}</h3></div>
-        <div className="Introduction">{this.props.scene.text}</div>
+        <div className="Introduction"><Markdown source={this.props.scene.text} /></div>
         {this.props.scene.question ?
           <div className="Question"><span className="Text">{this.props.scene.question}</span></div> :
           null}
