e6a6620816f1bdab2a420a9e625011ee32e4018b
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2017-12-25 19:12:50 +0200

    Tune styles

--- src/ui/SceneView.tsx
+++ src/ui/SceneView.tsx
@@ -13,8 +13,10 @@ export class SceneView extends React.Component<{
     return (
       <ImageTile className="Scene" url={this.props.scene.image}>
         <div className="Header"><h3>{this.props.scene.name}</h3></div>
-        <div className="Introdution">{this.props.scene.text}</div>
-        {this.props.scene.question ? <div className="Question">{this.props.scene.question}</div> : null}
+        <div className="Introduction">{this.props.scene.text}</div>
+        {this.props.scene.question ?
+          <div className="Question"><span className="Text">{this.props.scene.question}</span></div> :
+          null}
         {this.props.scene.choices && this.props.scene.choices.map(c =>
           <div className="Choice" key={c.sceneId}>
             <Link to={gameService.getSceneLink(this.props.game, c.sceneId)}>{c.text}</Link>
