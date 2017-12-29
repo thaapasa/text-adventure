8994acbfb51e0e4da09c91114abe0c3d21914c3e
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2017-12-29 22:42:51 +0200

    Tune Page layout to work for phones

--- src/ui/Page.tsx
+++ src/ui/Page.tsx
@@ -12,11 +12,11 @@ export default class Page extends React.Component<{
   public render() {
     return (
       <div className={'Page ' + (this.props.className || '')}>
-        <div className="Header" onClick={this.props.onTitleClick}>
-          <h1>{this.props.title}</h1>
-          <IconBar className="PageIcons" history={this.props.history} />
+        <div className="Page-Header" onClick={this.props.onTitleClick}>
+          <div className="Page-Title"><h1>{this.props.title}</h1></div>
+          <IconBar className="Page-Icons" history={this.props.history} />
         </div>
-        <div className="Content">{this.props.children}</div>
+        <div className="Page-Content">{this.props.children}</div>
       </div>
     );
   }
