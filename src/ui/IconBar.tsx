80b45fa4714c0a0d398f60db83ec7bebbcfc0c4c
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2017-12-29 12:11:19 +0200

    Clean up Icon props

--- src/ui/IconBar.tsx
+++ src/ui/IconBar.tsx
@@ -4,19 +4,28 @@ import { History } from 'history';
 import { MouseEvent } from 'react';
 const debug = require('debug')('game:icon-bar');
 
-function Icon({ icon, onClick }: {
-  readonly icon: string,
-  readonly onClick?: (event: MouseEvent<HTMLImageElement>) => void,
-}) {
-  return <img className="Icon" src={icon} onClick={onClick} />;
+interface IconProps {
+  readonly icon: string;
+  readonly onClick?: () => void;
+}
+
+class Icon extends React.Component<IconProps, {}> {
+  private onClick = (event: MouseEvent<HTMLImageElement>) => {
+    if (this.props.onClick) { this.props.onClick(); }
+    event.stopPropagation();
+  }
+
+  public render() {
+    return <img className="Icon" src={this.props.icon} onClick={this.onClick} />;
+  }
 }
 
 export class IconBar extends React.Component<{ className?: string, history: History }, {}> {
-  private goHome = (event: MouseEvent<HTMLImageElement>) => {
+  private goHome = () => {
     debug('Go home');
     this.props.history.push('/g/pelit');
-    event.stopPropagation();
   }
+
   public render() {
     return (
       <div className={'IconBar ' + (this.props.className || '')}>
