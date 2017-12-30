cbdee93780ed1e9f1c496b22e3fc3ff21b3ded6c
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2017-12-30 17:35:33 +0200

    Add action processing

--- src/ui/SceneView.tsx
+++ src/ui/SceneView.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './SceneView.css';
-import { Scene, Game, Item } from '../data/Game';
+import { Scene, Game, Item, Choice, Condition, Action } from '../data/Game';
 import { ImageTile } from './ImageTile';
 import { gameService } from '../data/GameService';
 import * as Markdown from 'react-markdown';
@@ -8,18 +8,44 @@ import { History } from 'history';
 import { ItemBar } from './ItemBar';
 const debug = require('debug')('game:scene-view');
 
-class Choice extends React.Component<{ history: History, title: string, link: string }, {}> {
+interface ChoiceViewProps {
+  readonly choice: Choice;
+  readonly history: History;
+  readonly game: Game;
+  readonly items: Item[];
+}
+
+class ChoiceView extends React.Component<ChoiceViewProps, {}> {
+  private filterItems = (action: Action, items: string[]): string[] => {
+    debug('Action', action);
+    switch (action.type) {
+      case 'receiveItem': return items.find(i => i === action.item) !== undefined ?
+        items : items.concat([ action.item ]);
+      case 'loseItem': return items.filter(i => i !== action.item);
+      default: return items;
+    }
+  }
   private selectLink = () => {
-    this.props.history.push(this.props.link);
+    let items: string[] = this.props.items.map(i => i.id);
+    if (this.props.choice.actions) {
+      this.props.choice.actions.forEach(a => {
+        items = this.filterItems(a, items);
+      });
+    }
+    debug('Filtered items', items);
+    const link = gameService.getSceneLink(this.props.game, this.props.choice.sceneId, items);
+    this.props.history.push(link);
   }
   public render() {
     return (
       <div className="Scene-Choice" onClick={this.selectLink}>
-        {this.props.title}
+        {this.props.choice.text}
       </div>
     );
   }
 }
+
+const noChoices: Choice[] = [];
 
 export class SceneView extends React.Component<{
   readonly game: Game;
@@ -32,7 +58,24 @@ export class SceneView extends React.Component<{
     debug('Scene', this.props.scene, this.props.items);
   }
 
+  private hasItem = (itemId: string): boolean => this.props.items.find(i => i.id === itemId) !== undefined;
+
+  private matchCondition = (condition: Condition): boolean => {
+    switch (condition.type) {
+      case 'hasItem': return this.hasItem(condition.item);
+      case 'doesNotHaveItem': return !this.hasItem(condition.item);
+      default: return false;
+    }
+  }
+
+  private canShowChoice = (choice: Choice): boolean => {
+    return choice.conditions && choice.conditions.length > 0 ?
+      choice.conditions.reduce((p, c) => p && this.matchCondition(c), true) : true;
+  }
+
   public render() {
+    const filteredChoices: Choice[] = this.props.scene.choices ?
+      this.props.scene.choices.filter(this.canShowChoice) : noChoices;
     return (
       <ImageTile className="Scene" url={this.props.scene.image}>
         <div className="Scene-Header"><h3>{this.props.scene.name}</h3></div>
@@ -45,12 +88,14 @@ export class SceneView extends React.Component<{
         {this.props.scene.question ?
           <div className="Scene-Question"><span className="Scene-Text">{this.props.scene.question}</span></div> :
           null}
-        {this.props.scene.choices && <div className="Scene-Choices">{this.props.scene.choices.map(c =>
-          <Choice
+        {filteredChoices.length > 0 && <div className="Scene-Choices">{filteredChoices.map(c =>
+          <ChoiceView
+            choice={c}
             key={c.sceneId}
             history={this.props.history}
-            title={c.text}
-            link={gameService.getSceneLink(this.props.game, c.sceneId, this.props.items.map(i => i.id))} />
+            game={this.props.game}
+            items={this.props.items}
+            />
         )}</div>}
       </ImageTile>
     );
